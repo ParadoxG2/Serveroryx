1c7ac14b8fe21323a56849875157f23acd14fcb7
landonm26301 <56404277+landonm26301@users.noreply.github.com>
2023-04-24 09:57:47 -0500

    aaaaaa
    aaaaaaaaa

--- src/Server.js
+++ src/Server.js
@@ -3,3 +3,15 @@
 ////////////////////////////////////////
 
 //Dependinceys
+const express = require('express');
+const app = express();
+
+    //[EXPRESS STARTUP]//
+INIT_Servonex();
+function INIT_Servonex()
+{
+    app.listen(3000);
+        //
+}
+
+
